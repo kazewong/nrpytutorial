28c9236737a11e5af119d87c01cc4e5ec91a2b19
kazewong1215 <kazewong.physics@gmail.com>
2020-02-04 10:25:41 -0500

    Add Initital data files

--- BSSN_SF/InitialData/Create_initial_data.py
+++ BSSN_SF/InitialData/Create_initial_data.py
@@ -2,35 +2,36 @@ import numpy as np
 from scipy.integrate import solve_ivp
 from scipy.interpolate import interp1d
 
-phi0 = 0.0006
-r0 = 5
-delta = 1
-q = 2
+phi0 = 0.0001
+r0 = 7
+delta = 5
+q = 2.
 
-def phi(r,phi0,r0,delta,q):
-  phi  = phi0*r**3*np.exp(-((r-r0)/delta)**q)
-  return phi
+def genSim(tag,phi0,r0,delta,q):
+	def phi(r,phi0,r0,delta,q):
+	  phi  = phi0*r**3*np.exp(-((r-r0)/delta)**q)
+	  return phi
+	
+	def dphidr(r,phi0,r0,delta,q):
+		a = 3*phi0*np.exp(-((r-r0)/delta)**q)*r**2
+		b = (phi0*np.exp(-((r-r0)/delta)**q)*q*r**3*((r-r0)/delta)**(q-1))/delta
+		return a-b
+	
+	def schwarzschild(M,r):
+		return 1.+M/2./r
+	
+	r_axis = np.arange(1e-10,100003*0.01,0.01)
+	dphi_interp = interp1d(r_axis,np.gradient(phi(r_axis,phi0,r0,delta,q),r_axis),bounds_error=False,fill_value='extrapolate')
+	
+	def RHS(r,y):
+		return [y[1],-np.pi*y[0]*dphidr(r,phi0,r0,delta,q)**2-2*y[1]/r]
+	
+	y = solve_ivp(RHS,[1e-10,1001],np.array([1.,0.]),t_eval=r_axis,rtol = 3e-14,atol = 1e-20).y
+	
+	np.savetxt('alpha'+tag+'.csv',np.ones(r_axis.size))
+	#np.savetxt('phi'+tag+'.csv',np.zeros(r_axis.size))#phi(r_axis,phi0,r0,delta,q))
+	np.savetxt('phi'+tag+'.csv',phi(r_axis,phi0,r0,delta,q))
+	np.savetxt('psi'+tag+'.csv',y[0])#schwarzschild(20,r_axis))
+	np.savetxt('Pi'+tag+'.csv',np.zeros(r_axis.size))
 
-def dphidr(r,phi0,r0,delta,q):
-	a = 3*phi0*np.exp(-((r-r0)/delta)**q)*r**2
-	b = (phi0*np.exp(-((r-r0)/delta)**q)*q*r**3*((r-r0)/delta)**(q-1))/delta
-	return a-b
 
-def schwarzschild(M,r):
-	return 1.+M/2./r
-
-r_axis = np.arange(1e-10,100003*0.01,0.01)
-#dphi_interp = interp1d(r_axis,np.gradient(phi(r_axis,phi0,r0,delta,q),r_axis),bounds_error=False,fill_value='extrapolate')
-
-def RHS(r,y):
-	return [y[1],-np.pi*y[0]*dphidr(r,phi0,r0,delta,q)**2-2*y[1]/r]
-
-y = solve_ivp(RHS,[1e-10,1001],np.array([1.,0.]),t_eval=r_axis,rtol = 3e-14,atol = 1e-20).y
-
-tag = 'BH1'
-
-
-np.savetxt('alpha'+tag+'.csv',np.ones(r_axis.size))
-np.savetxt('phi'+tag+'.csv',np.zeros(r_axis.size))#phi(r_axis,phi0,r0,delta,q))
-np.savetxt('psi'+tag+'.csv',schwarzschild(20,r_axis))
-np.savetxt('Pi'+tag+'.csv',np.zeros(r_axis.size))
