a4dd49a89ba4d9ed4059d8968463ff8b8defb255
Thomas <thomashelfer@live.de>
2020-02-13 14:23:56 -0500

    Added ADM trans to notebook

--- BSSN_SF/ADM_ID_function_string.py
+++ BSSN_SF/ADM_ID_function_string.py
@@ -7,14 +7,14 @@
 from outputC import *
 
 def ADM_ID_function_string(gammaCartDD,KCartDD,alphaCart,betaCartU,BCartU):
-    returnstring = "void BSSN_ID(FILE *out2D, REAL time, REAL xx0,REAL xx1,REAL xx2,REAL Cartxyz0,REAL Cartxyz1,REAL Cartxyz2,\n"
-    returnstring += "\tREAL *hDD00,REAL *hDD01,REAL *hDD02,REAL *hDD11,REAL *hDD12,REAL *hDD22,\n"
-    returnstring += "\tREAL *aDD00,REAL *aDD01,REAL *aDD02,REAL *aDD11,REAL *aDD12,REAL *aDD22,\n"
-    returnstring += "\tREAL *trK,\n"
-    returnstring += "\tREAL *lambdaU0,REAL *lambdaU1,REAL *lambdaU2,\n"
-    returnstring += "\tREAL *vetU0,REAL *vetU1,REAL *vetU2,\n"
-    returnstring += "\tREAL *betU0,REAL *betU1,REAL *betU2,\n"
-    returnstring += "\tREAL *alpha,REAL *cf) {\n"
+    returnstring = "void ADMCart_ID(FILE *out2D, double time, double xx0,double xx1,double xx2,double Cartxyz0,double Cartxyz1,double Cartxyz2,\n"
+    returnstring += "\tdouble hDD00,double hDD01,double hDD02,double hDD11,double hDD12,double hDD22,\n"
+    returnstring += "\tdouble aDD00,double aDD01,double aDD02,double aDD11,double aDD12,double aDD22,\n"
+    returnstring += "\tdouble trK,\n"
+    returnstring += "\tdouble lambdaU0,double lambdaU1,double lambdaU2,\n"
+    returnstring += "\tdouble vetU0,double vetU1,double vetU2,\n"
+    returnstring += "\tdouble betU0,double betU1,double betU2,\n"
+    returnstring += "\tdouble alpha,double cf) {\n"
     returnstring += "\tdouble gammaCartDD00;\n "
     returnstring += "\tdouble gammaCartDD01;\n "
     returnstring += "\tdouble gammaCartDD02;\n "
@@ -33,7 +33,7 @@ def ADM_ID_function_string(gammaCartDD,KCartDD,alphaCart,betaCartU,BCartU):
     returnstring += "\tdouble BCartU0;\n "
     returnstring += "\tdouble BCartU1;\n "
     returnstring += "\tdouble BCartU2;\n "
-    returnstring += "\tdouble alpha;\n "
+    returnstring += "\tdouble alphaCart;\n "
     returnstring += outputC([gammaCartDD[0][0], gammaCartDD[0][1], gammaCartDD[0][2], gammaCartDD[1][1], gammaCartDD[1][2], gammaCartDD[2][2],
                              KCartDD[0][0], KCartDD[0][1], KCartDD[0][2], KCartDD[1][1], KCartDD[1][2], KCartDD[2][2],
                              betaCartU[0], betaCartU[1], betaCartU[2],
@@ -43,7 +43,7 @@ def ADM_ID_function_string(gammaCartDD,KCartDD,alphaCart,betaCartU,BCartU):
                              "KCartDD00", "KCartDD01", "KCartDD02", "KCartDD11", "KCartDD12", "KCartDD22",
                              "betaCartU0", "betaCartU1", "betaCartU2",
                              "BCartU0", "BCartU1", "BCartU2",
-                             "alpha"], filename="returnstring",
+                             "alphaCart"], filename="returnstring",
                             params="preindent=1,CSE_enable=True,outCverbose=False",  # outCverbose=False to prevent
                             # enormous output files.
                             prestring="", poststring="")
@@ -52,6 +52,6 @@ def ADM_ID_function_string(gammaCartDD,KCartDD,alphaCart,betaCartU,BCartU):
     returnstring += '       KCartDD00, KCartDD01, KCartDD02, KCartDD11, KCartDD12, KCartDD22,  \n '
     returnstring += '       betaCartU0, betaCartU1, betaCartU2, \n '
     returnstring += '       BCartU0, BCartU1, BCartU2, \n '
-    returnstring += '       alpha) \n '
+    returnstring += '       alphaCart); \n '
     returnstring += "}\n"
     return returnstring
