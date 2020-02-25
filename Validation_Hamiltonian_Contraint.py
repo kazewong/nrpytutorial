cc511325f37f01284b2b83584beb2a452556b3fb
Thomas <thomashelfer@live.de>
2020-02-25 18:15:49 -0500

    improved code

--- Validation_Hamiltonian_Contraint.py
+++ Validation_Hamiltonian_Contraint.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 data = np.genfromtxt("BSSN_SF-output2D/quad_pot_2d_t-00000000.txt")
 
 DIM = 3
-N1 = int(len(data[:, 0]) / 4)
+N1 = int(len(data[:, 0])/4)
 
 time = data[:N1, 0]
 x = data[:N1, 1]
@@ -32,6 +32,7 @@ BCartU0 = data[:N1, 19]
 BCartU1 = data[:N1, 20]
 BCartU2 = data[:N1, 21]
 alphaCart = data[:N1, 22]
+hamreal = data[:N1,23]
 
 uu = data[:N1, 23]
 vv = data[:N1, 24]
@@ -54,6 +55,8 @@ d2gammaCartDD12dr2 = np.gradient(dgammaCartDD12dr, dr, edge_order=order)
 d2gammaCartDD22dr2 = np.gradient(dgammaCartDD22dr, dr, edge_order=order)
 
 gammaDD = np.zeros((N1, 3, 3))
+dgammaDDdr = np.zeros(( 3, 3))
+dgammaDDdx = np.zeros((N1, 3, 3, 3))
 KDD = np.zeros((N1, 3, 3))
 
 for i in range(N1):
@@ -67,6 +70,20 @@ for i in range(N1):
                             gammaCartDD12[i],
                             gammaCartDD22[i]]])
 
+    dgammaDDdr = np.array([[dgammaCartDD00dr[i],
+                            dgammaCartDD01dr[i],
+                            dgammaCartDD02dr[i]],
+                           [dgammaCartDD01dr[i],
+                            dgammaCartDD11dr[i],
+                            dgammaCartDD12dr[i]],
+                           [dgammaCartDD02dr[i],
+                            dgammaCartDD12dr[i],
+                            dgammaCartDD22dr[i]]])
+
+    dgammaDDdx[i][0] = x[i]/r[i] * dgammaDDdr
+    dgammaDDdx[i][1] = y[i]/r[i] * dgammaDDdr
+    dgammaDDdx[i][2] = z[i]/r[i] * dgammaDDdr
+
     KDD[i] = np.array([[KCartDD00[i], KCartDD01[i], KCartDD02[i]],
                        [KCartDD01[i], KCartDD11[i], KCartDD12[i]],
                        [KCartDD02[i], KCartDD12[i], KCartDD22[i]]])
@@ -77,16 +94,16 @@ gammaUU = np.linalg.inv(gammaDD)
 TrK = np.zeros(N1)
 
 for i in range(N1):
-    for k in range(3):
-        for d in range(3):
+    for k in range(DIM):
+        for d in range(DIM):
             TrK[i] += KDD[i][k, d] * gammaUU[i][k, d]
 
 KijKij = np.zeros(N1)
 for i in range(N1):
-    for k in range(3):
-        for d in range(3):
-            for l in range(3):
-                for m in range(3):
+    for k in range(DIM):
+        for d in range(DIM):
+            for l in range(DIM):
+                for m in range(DIM):
                     KijKij[i] += KDD[i][k, d] * KDD[i][l, m] * \
                         gammaUU[i][k, l] * gammaUU[i][d, m]
 
@@ -101,46 +118,65 @@ Vofu = 0
 
 vv2 = vv * vv
 psi2 = 0
-for i in range(DIM):
+for ind in range(N1):
     for j in range(DIM):
         for k in range(DIM):
-            psi2 += gammaUU[i][j, k] * uu_dD[i][j] * uu_dD[i][k]
+            psi2 += gammaUU[ind][j, k] * uu_dD[ind][j] * uu_dD[ind][k]
 
 rho = 0.5 * (vv2 + psi2) + Vofu
 
-RicciScalar = (
-    2 * (
-        dgammaCartDD00dr * dgammaCartDD12dr - 2 * d2gammaCartDD12dr2 * gammaCartDD00) * gammaCartDD12**3 + gammaCartDD12**2 * (
-            dgammaCartDD12dr**2 * gammaCartDD00 + 3 * dgammaCartDD11dr * dgammaCartDD22dr * gammaCartDD00 - 4 * dgammaCartDD02dr * dgammaCartDD12dr * gammaCartDD01 + (
-                -4 * dgammaCartDD01dr * dgammaCartDD12dr + 8 * d2gammaCartDD12dr2 * gammaCartDD01) * gammaCartDD02 - dgammaCartDD00dr * dgammaCartDD22dr * gammaCartDD11 + 2 * d2gammaCartDD22dr2 * gammaCartDD00 * gammaCartDD11 + (
-                    -(
-                        dgammaCartDD00dr * dgammaCartDD11dr) + 2 * d2gammaCartDD11dr2 * gammaCartDD00) * gammaCartDD22) + gammaCartDD11**2 * (
-                            dgammaCartDD22dr**2 * gammaCartDD00 - 2 * dgammaCartDD02dr * dgammaCartDD22dr * gammaCartDD02 + 2 * d2gammaCartDD22dr2 * gammaCartDD02**2 + (
-                                dgammaCartDD00dr * dgammaCartDD22dr - 2 * d2gammaCartDD22dr2 * gammaCartDD00) * gammaCartDD22) + gammaCartDD11 * (
-                                    -(
-                                        dgammaCartDD22dr**2 * gammaCartDD01**2) + 2 * dgammaCartDD12dr * dgammaCartDD22dr * gammaCartDD01 * gammaCartDD02 + (
-                                            -3 * dgammaCartDD12dr**2 + 2 * dgammaCartDD11dr * dgammaCartDD22dr) * gammaCartDD02**2 + (
-                                                3 * dgammaCartDD12dr**2 * gammaCartDD00 - dgammaCartDD11dr * dgammaCartDD22dr * gammaCartDD00 - 2 * dgammaCartDD01dr * dgammaCartDD22dr * gammaCartDD01 + 2 * d2gammaCartDD22dr2 * gammaCartDD01**2 - 2 * dgammaCartDD02dr * dgammaCartDD11dr * gammaCartDD02 + 2 * d2gammaCartDD11dr2 * gammaCartDD02**2) * gammaCartDD22 + (
-                                                    dgammaCartDD00dr * dgammaCartDD11dr - 2 * d2gammaCartDD11dr2 * gammaCartDD00) * gammaCartDD22**2) + gammaCartDD22 * (
-                                                        gammaCartDD01**2 * (
-                                                            -3 * dgammaCartDD12dr**2 + 2 * dgammaCartDD11dr * dgammaCartDD22dr + 2 * d2gammaCartDD11dr2 * gammaCartDD22) + 2 * dgammaCartDD11dr * gammaCartDD01 * (
-                                                                dgammaCartDD12dr * gammaCartDD02 - dgammaCartDD01dr * gammaCartDD22) + dgammaCartDD11dr**2 * (
-                                                                    -gammaCartDD02**2 + gammaCartDD00 * gammaCartDD22)) + 2 * gammaCartDD12 * (
-                                                                        -2 * dgammaCartDD12dr * dgammaCartDD22dr * gammaCartDD00 * gammaCartDD11 + gammaCartDD02**2 * (
-                                                                            dgammaCartDD11dr * dgammaCartDD12dr - 2 * d2gammaCartDD12dr2 * gammaCartDD11) - 2 * dgammaCartDD11dr * dgammaCartDD12dr * gammaCartDD00 * gammaCartDD22 - dgammaCartDD00dr * dgammaCartDD12dr * gammaCartDD11 * gammaCartDD22 + 2 * d2gammaCartDD12dr2 * gammaCartDD00 * gammaCartDD11 * gammaCartDD22 + gammaCartDD01**2 * (
-                                                                                dgammaCartDD12dr * dgammaCartDD22dr - 2 * d2gammaCartDD12dr2 * gammaCartDD22) + gammaCartDD01 * (
-                                                                                    dgammaCartDD02dr * dgammaCartDD22dr * gammaCartDD11 + (
-                                                                                        dgammaCartDD02dr * dgammaCartDD11dr + 2 * dgammaCartDD01dr * dgammaCartDD12dr) * gammaCartDD22) + gammaCartDD02 * (
-                                                                                            (dgammaCartDD12dr**2 - 3 * dgammaCartDD11dr * dgammaCartDD22dr) * gammaCartDD01 + (
-                                                                                                2 * dgammaCartDD02dr * dgammaCartDD12dr + dgammaCartDD01dr * dgammaCartDD22dr - 2 * d2gammaCartDD22dr2 * gammaCartDD01) * gammaCartDD11 + (
-                                                                                                    dgammaCartDD01dr * dgammaCartDD11dr - 2 * d2gammaCartDD11dr2 * gammaCartDD01) * gammaCartDD22))) / (
-                                                                                                        2. * (
-                                                                                                            gammaCartDD02**2 * gammaCartDD11 - 2 * gammaCartDD01 * gammaCartDD02 * gammaCartDD12 + gammaCartDD01**2 * gammaCartDD22 + gammaCartDD00 * (
-                                                                                                                gammaCartDD12**2 - gammaCartDD11 * gammaCartDD22))**2)
+ChristoffelUDD = np.zeros((N1,DIM,DIM,DIM))
+for ind in range(N1):
+    for i in range(DIM):
+        for k in range(DIM):
+            for l in range(DIM):
+                for m in range(DIM):
+                    ChristoffelUDD[ind][i,k,l] += 0.5  * gammaUU[ind][i,m] * ( dgammaDDdx[ind][l,m,k]  + dgammaDDdx[ind][k,m,l] - dgammaDDdx[ind][m,k,l])
 
+dChristoffelUDDdx = np.zeros((N1,DIM,DIM,DIM,DIM))
+dChristoffelUDDdr = np.zeros((N1,DIM,DIM,DIM))
+for i in range(DIM):
+    for k in range(DIM):
+        for l in range(DIM):
+            dChristoffelUDDdr[:][i,k,l] = np.gradient(ChristoffelUDD[:][i,k,l], dr, edge_order=order)
 
+for ind in range(N1):
+    for i in range(DIM):
+        for k in range(DIM):
+            for l in range(DIM):
+                dChristoffelUDDdx[ind][0,i,k,l] += x[ind]/r[ind] * dChristoffelUDDdr[ind][i,k,l] 
+                dChristoffelUDDdx[ind][1,i,k,l] += y[ind]/r[ind] * dChristoffelUDDdr[ind][i,k,l] 
+                dChristoffelUDDdx[ind][2,i,k,l] += z[ind]/r[ind] * dChristoffelUDDdr[ind][i,k,l]  
+
+RiemannUDDD = np.zeros((N1,DIM,DIM,DIM,DIM))
+for ind in range(N1):
+    for l in range(DIM):
+        for i in range(DIM):
+            for j in range(DIM):
+                for k in range(DIM):
+                    RiemannUDDD[ind][l,i,j,k] += dChristoffelUDDdx[ind][j,l,i,k] - dChristoffelUDDdx[ind][k,l,i,j]  
+                    for s in range(DIM):
+                        RiemannUDDD[ind][l,i,j,k] += ChristoffelUDD[ind][l,j,s]*ChristoffelUDD[ind][s,i,k] - ChristoffelUDD[ind][l,k,s] * ChristoffelUDD[ind][s,i,j]
+
+RicciTensorDD = np.zeros((N1,DIM,DIM))
+
+for ind in range(N1):
+    for l in range(DIM):
+        for i in range(DIM):
+            for j in range(DIM):
+                RicciTensorDD[ind][l,i] += RiemannUDDD[ind][j,i,j,k]
+
+RicciScalar = np.zeros(N1)
+for ind in range(N1):
+    for l in range(DIM):
+        for i in range(DIM):
+            RicciScalar[ind] += RicciTensorDD[ind][l,i]*gammaUU[ind][l][i] 
 
 HamConstraint = RicciScalar + KijKij - TrK**2 - 16*np.pi*rho
 
-plt.plot(HamConstraint)
+#plt.plot(rho, label =  "rho ",linestyle = ":" )
+#plt.plot(HamConstraint,label = "Calculated here ")
+#plt.plot(hamreal, label =  "Calculated internally " )
+plt.plot(RicciScalar, label =  "Ricci ",linestyle = "--" )
+plt.legend()
 plt.show()
